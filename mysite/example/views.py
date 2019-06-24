fd57394a6b75272c445c0463d783dc84d8e58dbb
sijiapei <m18696182661@163.com>
2019-06-24 15:22:18 +1000

    week2

--- mysite/example/views.py
+++ mysite/example/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 import json
-from .models import *
+from.models import *
+import os,sys
 
 # Create your views here.
 
@@ -34,3 +35,35 @@ def example_post(request):
 			return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
 	else:
 		return HttpResponse("ONLY POST REQUESTS")
+
+	
+@csrf_exempt
+def fib(request):
+    jsob = {"startNumber":0,"length":10}
+    log = []
+    if request.method == "POST":
+        try:
+            data = request.POST["data"]
+            received = json.loads(data)
+            jsob.update(received)
+            #111111111111111111111111111111111
+            startNumber = int(jsob["startNumber"])
+            length = int(jsob["length"])
+            loop = range(length)
+            numarray = []
+            fitbon = startNumber
+            addno = 1
+            for i in loop:
+                numarray.append(fitbon)
+                fitbon = fitbon + addno
+                addno = fitbon + addno
+
+            return JsonResponse({"fib":numarray})
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            other = sys.exc_info()[0].__name__
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            errorType = str(exc_type)
+            return JsonResponse({"isError": True, "error":str(e), "errorType":errorType, "function":fname, "line":exc_tb.tb_lineno, "log":log})
+        else:
+            return JsonResponse(jsob)
--- mysite/wombat/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WombatConfig(AppConfig):
-    name = 'wombat'
